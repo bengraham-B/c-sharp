a59758e14d7d3f338dfa2cfd04145af0844fab2a
Ben Graham <bengraham@Bens-MacBook-Air.local>
2023-06-28 22:42:44 +0200

    Learning C#

---
+++ data_types/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(){
+
+       string phrase = "Goose_CPT";
+       char grade = 'S';
+       int age = 33;
+       float least_accurate = 2.3F;
+       double medium_accurate = 2.33D;
+       decimal most_accurate = 2.369240M;
+       bool isGay = false;
+
+       Console.WriteLine( phrase + " " + grade);
+
+    }
+}
