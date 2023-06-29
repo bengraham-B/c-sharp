cf3f4c43c24e1391d5851e7d0f91b9fb204178c8
Ben Graham <bengraham@Bens-MacBook-Air.local>
2023-06-29 17:14:23 +0200

    From Mac: 1:09:00

---
+++ building_a_calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(){
+
+        string characterName = "Goerge";
+        int characterAge = 70;
+
+        Console.WriteLine("There was oncde a man named " + characterName); 
+        Console.WriteLine("He was " + characterAge + " Years old"); 
+        Console.WriteLine("He really liked the name " + characterName); 
+        Console.WriteLine("He did not like being seventy " + characterAge); 
+        
+        characterName = "Mike";
+        Console.WriteLine("He changed his name to " + characterName); 
+
+        Console.ReadLine();
+    }
+}
