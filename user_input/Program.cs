b54771b546eec405cb64a3c15c0fa45171a42749
Ben Graham <grahamben7@gmail.com>
2023-06-29 12:54:26 +0200

    1:07:38

--- user_input/Program.cs
+++ user_input/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Enter your Name: ");
+        string name, age;
+        //int age;
+        name = Console.ReadLine();
+        Console.WriteLine("Hello " + name);
+
+        Console.WriteLine("What is your age?");
+        age = Console.ReadLine();
+        Console.WriteLine(age + " That is a preety fun age");
+
+        Console.ReadLine();
+
+    }
+}
--- working_with_numbers/Program.cs
+++ working_with_numbers/Program.cs
@@ -2,9 +2,16 @@
 
 using System;
 
-class Program
-{
-    static void Main(){
+class Program {
+    static void Main() {
+
+        // Math Methods
+        Console.WriteLine(Math.Abs(40))
+        Console.WriteLine(Math.Abs(40, 2))
+        Console.WriteLine(Math.Sqrt(40))
+        Console.WriteLine(Math.Max(40, 69)) // Returns Largest Number
+        Console.WriteLine(Math.Min(40, 69)) // Returns Smallest Number
+        Console.WriteLine(Math.Round(4.69)) //Rounding Numbers
 
         Console.WriteLine("Addition " + 4 + 7);
         Console.WriteLine("Subtraction " + (4 - 7));
