eb587b96a887ac77a423bb9247bf396d86fc6625
Ben Graham <bengraham@Bens-MacBook-Air.local>
2023-06-29 21:11:23 +0200

    From Mac: 1:25:00

---
+++ arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+      
+        // Making an array
+        int[] luckyNumbers = { 7, 33, 69, 420, 69420 };
+        
+        Console.WriteLine(luckyNumbers[3]);
+        Console.WriteLine("^_^");
+
+    }
+}
